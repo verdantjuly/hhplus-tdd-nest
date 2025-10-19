e6ec5b58b6530c6b61222a7151700ed43e0d04bc
verdantjuly <verdantjuly@kakao.com>
2025-10-20 07:45:14 +0900

    [Refactor] TransactionType 적용, PointHistoryDB 반영, 합산 결과 return
    - TransactionType을 적용해 타입별로 더해지거나 빼지게 함
    - PointHistoryDB에 insert와 동시에 저장되게 함
    - 기존에 입력값을 리턴하던 것을 합산 결과를 리턴하게 함.

--- src/point/point.service.spec.ts
+++ src/point/point.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PointService } from './point.service';
 import { BadRequestException } from '@nestjs/common';
 import { UserPointTable } from 'src/database/userpoint.table';
+import { PointHistoryTable } from 'src/database/pointhistory.table';
+import { TransactionType } from './point.model';
 
 /*
 
@@ -18,11 +20,11 @@ describe('PointService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PointService, UserPointTable],
+      providers: [PointService, UserPointTable, PointHistoryTable],
     }).compile();
 
     pointService = module.get<PointService>(PointService);
-    await pointService.insertOrUpdate(777, 777);
+    await pointService.insertOrUpdate(777, 777, TransactionType.CHARGE);
   });
 
   it('should be defined', () => {
@@ -52,26 +54,39 @@ describe('PointService', () => {
   describe('givenUserId_whenInsertOrUpdate_thenReturnUserPoint', () => {
     // userId가 int가 아니면 에러 발생
     it('userId not int should throw error', async () => {
-      // @ts-ignore
-      const insertOrUpdate = pointService.insertOrUpdate('nyx', 77);
+      const insertOrUpdate = pointService.insertOrUpdate(
+        // @ts-ignore
+        'nyx',
+        77,
+        TransactionType.CHARGE,
+      );
 
       await expect(insertOrUpdate).rejects.toBeInstanceOf(Error);
     });
 
-    // amount가 int가 아니면 에러 발생
-    it('amount not int should throw error', async () => {
-      // @ts-ignore
-      const insertOrUpdate = pointService.insertOrUpdate(77, 'nyx');
-
-      await expect(insertOrUpdate).rejects.toBeInstanceOf(Error);
-    });
-
-    // 특정 UserId, Amount에 대한 정확한 UserPoint 반환
+    // CHARGE에 대한 정확한 결과 반환
     it('Exact UserPoint about specific UserId', async () => {
-      const insertOrUpdate = pointService.insertOrUpdate(123, 777);
+      const insertOrUpdate = pointService.insertOrUpdate(
+        777,
+        1,
+        TransactionType.CHARGE,
+      );
       await expect(insertOrUpdate).resolves.toStrictEqual({
-        id: 123,
-        point: 777,
+        id: 777,
+        point: 778,
+        updateMillis: expect.anything(),
+      });
+    });
+    // USE에 대한 정확한 결과 반환
+    it('Exact UserPoint about specific UserId', async () => {
+      const insertOrUpdate = pointService.insertOrUpdate(
+        777,
+        777,
+        TransactionType.USE,
+      );
+      await expect(insertOrUpdate).resolves.toStrictEqual({
+        id: 777,
+        point: 0,
         updateMillis: expect.anything(),
       });
     });
--- src/point/point.service.ts
+++ src/point/point.service.ts
@@ -1,18 +1,34 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-
+import { PointHistoryTable } from 'src/database/pointhistory.table';
 import { UserPointTable } from 'src/database/userpoint.table';
+import { TransactionType } from './point.model';
 
 @Injectable()
 export class PointService {
-  constructor(private readonly userDb: UserPointTable) {}
+  constructor(
+    private readonly userDb: UserPointTable,
+    private readonly historyDb: PointHistoryTable,
+  ) {}
   async selectById(id: number) {
     return await this.userDb.selectById(id);
   }
 
-  async insertOrUpdate(id: number, amount: number) {
-    if (!Number.isInteger(amount)) {
-      throw new Error('Amount is not valid int');
+  async insertOrUpdate(
+    id: number,
+    amount: number,
+    transactionType: TransactionType,
+  ) {
+    const origin = await this.userDb.selectById(id);
+    if (transactionType === TransactionType.USE) {
+      amount = -amount;
     }
-    return await this.userDb.insertOrUpdate(id, amount);
+    const result = await this.userDb.insertOrUpdate(id, origin.point + amount);
+    await this.historyDb.insert(
+      id,
+      amount,
+      transactionType,
+      result.updateMillis,
+    );
+    return await this.selectById(id);
   }
 }
